b43f4d1dfc10a05105d74dd2fc7d85801cbe451f
CharlieOuthwaite <charlotteouthwaite@gmail.com>
2022-11-01 17:37:32 +0000

    Started coding up exploration of primary veg sites

---
+++ 11_Explorations_primaryveg.R
@@ -0,0 +1,76 @@
+##%######################################################%##
+#                                                          #
+#### Exploration of data from primary vegetation sites  ####
+#                                                          #
+##%######################################################%##
+
+
+# Charlie Outhwaite 25/10/22
+
+# In this script, I will look into the spread of climate data available for 
+# sites in primary vegetation. This is to check whether some studies with more
+# broadly distributed sites are driving the negative/positive trends in this 
+# land use. 
+
+# ensure environment is clear
+rm(list = ls())
+
+# load libraries
+library(ggplot2)
+
+# directories
+datadir <- "5_RunLUIClimateModels/"
+outdir <- "11_Explore_primveg/"
+if(!dir.exists(outdir)) dir.create(outdir)
+
+# load in the dataset that includes the climate info
+pred <- readRDS(file = paste0(datadir,"PREDICTSSitesClimate_Data.rds"))
+pred <- droplevels(pred)
+
+# number of unique studies
+length(unique(pred$SS)) # 257
+
+# number of sites within studies
+site_tab <- data.frame(table(pred$SS))
+range(site_tab$Freq) # number of sites ranges from 1 to 680
+
+
+#### plot sites within a study ####
+
+
+# for each study, plot sites
+
+# plot the raster in ggplot
+map.world <- map_data('world')
+
+# i <- site_tab$Var1[1]
+
+for(i in site_tab$Var1){
+  
+  # subset the data
+  datsub <- pred[pred$SS == i & pred$Predominant_land_use == "Primary vegetation", ]
+  
+  if(nrow(datsub) == 0) next
+  
+  
+  # create and save a map
+  p1 <-ggplot() +
+    geom_map(data=map.world, map=map.world,
+             aes(x=long, y=lat, group=group, map_id=region),
+             fill= "grey", colour="grey", size=0.2) +
+    geom_point(data = datsub, aes(x = Longitude, y = Latitude, colour = factor(Order)), shape = 20, size = 2) +
+    theme_bw() + 
+    theme(axis.title = element_blank(), 
+          axis.text = element_blank(),
+          axis.ticks = element_blank(), 
+          legend.title = element_blank())
+  
+  ggsave(p1, filename = paste0(outdir, i, "pointmap.png"))
+  
+}
+
+
+
+
+
+
