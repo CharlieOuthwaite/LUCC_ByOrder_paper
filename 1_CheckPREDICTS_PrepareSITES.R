9a8fd469479f777c7627cb4e03485339d62ed375
CharlieOuthwaite <charlotteouthwaite@gmail.com>
2023-11-01 17:59:17 +0000

    Cleaned code and rescaled abundance data.

--- 1_CheckPREDICTS_PrepareSITES.R
+++ 1_CheckPREDICTS_PrepareSITES.R
@@ -18,7 +18,8 @@ plotDir <- "1_CheckPrepareData/Plots/"
 if(!dir.exists(plotDir)) dir.create(plotDir)
 
 # Load required libraries
-packages <- c("predictsFunctions","patchwork", "dplyr", "ggplot2", "yarg", "lme4", "gt", "broom.mixed", "MASS","webshot")
+packages <- c("predictsFunctions","patchwork", "dplyr", "ggplot2", "yarg", "lme4", "gt",
+              "broom.mixed", "MASS","webshot")
 suppressWarnings(suppressMessages(lapply(packages, require, character.only = TRUE)))
 
 # source in additional functions
@@ -37,11 +38,12 @@ predicts <- ReadPREDICTS(predicts.path)
 predicts <- predicts[(predicts$Class=="Insecta"),]
 # 935078 obs. of 67 variables
 
-# Correct effort-sensitive abundance measures (assumes linear relationship between effort and recorded abundance)
+# Correct effort-sensitive abundance measures (assumes linear relationship between 
+# effort and recorded abundance)
 predicts <- CorrectSamplingEffort(diversity = predicts)
 # Correcting 0 missing sampling effort values
 # Re-scaling sampling effort
-# Correcting 870378 values for sensitivity to sampling effort # matches Outhwaite et al.
+# Correcting 870378 values for sensitivity to sampling effort 
 
 # check diversity metrics
 table(predicts$Diversity_metric)
@@ -51,7 +53,8 @@ table(predicts$Diversity_metric)
 predicts <- predicts[!predicts$Diversity_metric == "percent cover", ]
 # 934845 obs. of 67 variables
 
-# Merge sites that have the same coordinates, from the same study and same taxonomic family (e.g. multiple traps on a single transect)
+# Merge sites that have the same coordinates, from the same study and same taxonomic 
+# family (e.g. multiple traps on a single transect)
 predicts <- predictsFunctions::MergeSites(diversity = predicts)
 # 826292 obs. of 67 variables
 
@@ -60,7 +63,7 @@ predicts <- droplevels(predicts[(predicts$Order!=""),])
 # 826016 obs. of 67 variables
 
 # check
-table(predicts$Order) # 826016 obs. of 67 variables
+table(predicts$Order) 
 
 # keep top five orders 
 predicts <- predicts %>% filter(Order %in% c("Hymenoptera", "Coleoptera", "Lepidoptera", "Diptera", "Hemiptera")) %>% droplevels()
@@ -73,7 +76,7 @@ predicts$Order <- as.factor(predicts$Order)
 
 # Split predicts into separate data frames according to insect Order 
 
-# use split function to split the predicts data frame into 6 data frames (1/Order)
+# use split function to split the predicts data frame into 5 data frames (1/Order)
 OrderName <- paste0("",predicts$Order)
 
 by_Order <- split(predicts,OrderName)
@@ -81,6 +84,7 @@ by_Order <- split(predicts,OrderName)
 # extract data frames from list into global environment
 list2env(by_Order,globalenv())
 
+# drop levels and calculate site level metrics
 Coleoptera <- droplevels(Coleoptera)
 Coleoptera <- SiteMetrics(diversity = Coleoptera,
                           extra.cols = c("Predominant_land_use",
@@ -160,21 +164,22 @@ sites$LUI <- dplyr::recode(sites$LUI,
 
 # 10746 obs. of 29 variables
 
-
 # remove the urban sites and sites that are NA in LUI
 sites <- sites[!sites$LUI == "Urban", ]
 sites <- sites[!is.na(sites$LUI), ]
-
 # 8890 obs. of 29 variables
 
 sites <- droplevels(sites)
 
+# rescale abundance values
+sites <- RescaleAbundance(sites)
+
+# add another column without rescaling for testing later
 # transform abundance values 
-sites$LogAbund <- log(sites$Total_abundance+0.01)
+sites$LogAbund_noRS <- log(sites$Total_abundance+0.01)
 
 # Remove sites without coordinates
 sites <- sites[!is.na(sites$Latitude), ]
-
 # 8884 obs. of 30 variables
 
 # create a new variable designating sites as Tropical or Non-tropical
@@ -187,16 +192,23 @@ sites <- sites %>%
 # save the prepared dataset
 saveRDS(object = sites,file = paste0(outDir,"PREDICTSSiteData.rds")) 
 
-#### 3a. Summarise data by order ####
+##%######################################################%##
+#                                                          #
+####             3. Summarise data by order             ####
+#                                                          #
+##%######################################################%##
 
+
+# n unique sites across the dataset
 length(unique(sites$SSBS)) # 6014
-
+# n unique studies across the dataset
 length(unique(sites$SS)) # 254
 
 
+# This creates a summary table splitting by Order and land use to show n studies and n sites
 sites_summary <- sites %>%
   group_by(Order) %>% 
-  mutate(LogAbund = ifelse(is.na(LogAbund), 0, LogAbund)) %>% # replace NA values with 0 for ease of summarising
+  #mutate(LogAbund = ifelse(is.na(LogAbund), 0, LogAbund)) %>% # replace NA values with 0 for ease of summarising
   summarise(Sites = length(SSBS),
             Studies = n_distinct(SS),
             Species = n_distinct(Best_guess_binomial),
@@ -208,13 +220,13 @@ sites_summary <- sites %>%
             studies_low = n_distinct(SS[LUI == "Agriculture_Low"]),
             sites_high = length(LUI[LUI == "Agriculture_High"]),
             studies_high = n_distinct(SS[LUI == "Agriculture_High"]),
-            Abundance = sum(LogAbund>=0) ,
+            Abundance = sum(!is.na(LogAbund)) ,
             SpeciesRichness = sum(Species_richness>=0)) %>%
   ungroup() %>%  
   arrange(desc(Sites))%>%
   gt() %>%
-  tab_header(title = "Data spread across land-uses"
-  ) %>%
+  # tab_header(title = "Data spread across land-uses"
+  # ) %>%
   tab_spanner(
     label = "Total",
     columns = c(Sites,Studies,Species)
@@ -266,107 +278,24 @@ gtsave(sites_summary,"sites_summary_SuppTab1.png",path = outDir)
 # save as csv
 write.csv(sites_summary,file = paste0(outDir, "sites_summary_SuppTab1.csv"))
 
-# #### 3b. Summarize data by order and latitudinal realm ####
-# 
-# lat_summary <- sites %>%
-#   group_by(Order,Realm) %>%
-#   mutate(LogAbund = ifelse(is.na(LogAbund), 0, LogAbund)) %>% # replace NA values with 0 for ease of summarising
-#   summarise(Sites = length(SSBS),
-#             Studies = n_distinct(SS),
-#             Species = n_distinct(Best_guess_binomial),
-#             sites_1 = length(LUI[LUI == "Primary vegetation"]) ,
-#             studies_1 = n_distinct(SS[LUI == "Primary vegetation"]),
-#             sites_2 = length(LUI[LUI == "Secondary vegetation"]),
-#             studies_2 = n_distinct(SS[LUI == "Secondary vegetation"]),
-#             sites_low = length(LUI[LUI == "Agriculture_Low"]),
-#             studies_low = n_distinct(SS[LUI == "Agriculture_Low"]),
-#             sites_high = length(LUI[LUI == "Agriculture_High"]),
-#             studies_high = n_distinct(SS[LUI == "Agriculture_High"]),
-#             Abundance = sum(LogAbund>0) ,
-#             SpeciesRichness = sum(Species_richness>0)) %>%
-#   ungroup() %>%
-#   gt() %>%
-#   tab_header(title = "Data spread across land-uses, by latitudinal realm"
-#   ) %>%
-#   tab_spanner(
-#     label = "Total",
-#     columns = c(Sites,Studies,Species)
-#   )  %>%
-#   tab_spanner(
-#     label = "Primary Vegetation",
-#     columns = c(sites_1,studies_1)
-#   ) %>%
-#   tab_spanner(
-#     label = "Secondary vegetation",
-#     columns = c(sites_2,studies_2)
-#   ) %>%
-#   tab_spanner(
-#     label = "Low-intensity agriculture",
-#     columns = c(sites_low,studies_low)
-#   ) %>%
-#   tab_spanner(
-#     label = "High-intensity agriculture",
-#     columns = c(sites_high,studies_high)
-#   )  %>%
-#   tab_spanner(
-#     label = "Diversity Metric",
-#     columns = c(Abundance,SpeciesRichness)
-#     )  %>% 
-#   cols_align(
-#     align = "center",
-#     columns = c(Order,Realm,Sites,Studies, Species,sites_1,studies_1,sites_2,studies_2,sites_low,studies_low,sites_high,studies_high,Abundance,SpeciesRichness)
-#   )%>%
-#   cols_label(
-#     Order = "Order",
-#     Realm = "Realm",
-#     Sites = "Sites",
-#     Studies = "Studies",
-#     Species = "Species",
-#     sites_1 = "Sites",
-#     studies_1 = "Studies",
-#     sites_2 = "Sites",
-#     studies_2 = "Studies",
-#     sites_low = "Sites",
-#     studies_low = "Studies",
-#     sites_high = "Sites",
-#     studies_high = "Studies",
-#     Abundance = "Abundance",
-#     SpeciesRichness = "Species richness"
-#   )
-
-# # save table
-# gtsave(lat_summary,"lat_summary_allorders.png",path = outDir)
-# 
-# # save as csv
-# write.csv(lat_summary,file = paste0(outDir, "lat_summary_allorders.csv"))
-
-
-
-length(unique(sites$SS))
-# 254 studies
-
-length(unique(sites$SSBS))
-# 6014 sites
-
 # assess the record level dataset
+# subset to those sites and orders selected
 pred_sub <- predicts[predicts$SSBS %in% sites$SSBS & predicts$Order %in% sites$Order, ]
 pred_sub <- droplevels(pred_sub)
 nrow(pred_sub) # 741933 records
 table(pred_sub$Order)
 # Coleoptera     Diptera   Hemiptera Hymenoptera Lepidoptera 
 #     377237       24149       45217      144474      150856
+
 table(pred_sub$Predominant_land_use)
 length(unique(pred_sub$SS)) # 254
 length(unique(pred_sub$SSBS)) # 6014
-length(unique(pred_sub$Species)) # 4324
-
-
-length(unique(pred_sub$Taxon_name_entered)) # 17218
+length(unique(pred_sub$Taxon_name_entered)) # 17218 "species"
 
 # determine unique taxa counts
 species <- unique(pred_sub[,c('Order',"Family", 'Taxon_name_entered')])
 
-# order level counts
+# order level counts of taxon names supplied
 order.counts <- tapply(X = species$Taxon_name_entered,
                        INDEX = species$Order,
                        FUN = function(sp) length(unique(sp)))
@@ -375,7 +304,7 @@ order.counts <- tapply(X = species$Taxon_name_entered,
 #       6037        1477        1302        4528        3874
 
 
-# family level counts
+# Order level counts of n families
 family.counts <- tapply(X = species$Family,
                        INDEX = species$Order,
                        FUN = function(sp) length(unique(sp)))
@@ -407,16 +336,9 @@ shp_df <- broom::tidy(map.borders1, region = "SUBREGION")
 
 # map of sites, no colour
 p_map1.1 <- ggplot() +
-  # geom_map(data=map.world, map=map.world,
-  #          aes(x=long, y=lat, group=group, map_id=region),
-  #          fill= "grey", colour="grey", size=0.2) +
   geom_polygon(data = shp_df, aes(x = long, y = lat, group = group), alpha = 0.5, col = "black", fill = c("grey")) +
-  # scale_fill_paletteer_d("ggsci::default_igv") +
   coord_fixed() +
-  #labs(fill = "Order") +
-  #geom_point(data = sites, aes(x = Longitude, y = Latitude, col = Order), shape = 20, size=2, position=position_jitter(width=1, height=1, seed = 1)) +
   geom_point(data = sites, aes(x = Longitude, y = Latitude, col = Order), shape = 21, size=2, col = c("#A4D3EE"), fill = c("#104E8B")) +
-  #scale_colour_manual(values=c("#0F6B99FF","#FFD8B2FF","#A3CC51FF","#8F7EE5FF","#990F0FFF"))+ # option, colouring points by Order
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         axis.text = element_blank(),
@@ -443,7 +365,6 @@ mapdf <- sites %>% dplyr::select(UN_subregion,Order,Longitude,Latitude) %>% # fi
             Hemiptera = length(Order[Order == "Hemiptera"]),
             Hymenoptera = length(Order[Order == "Hymenoptera"]),
             Lepidoptera = length(Order[Order == "Lepidoptera"]),
-            long = mean(Longitude), lat = mean(Latitude), # calculate the mean coordinates, for plotting the piecharts
             radius = 25*sqrt(length(UN_subregion)/nrow(sites))) # calculate the radius for each subregion
 
 
@@ -459,18 +380,15 @@ mapdf2$UN_subregion <- factor(mapdf2$UN_subregion)
 mapdf2$Order <- factor(mapdf2$Order) 
 
 p_bar <- ggplot(data=mapdf2) +
-  # scale_fill_manual(values=c("#2271B2","#F0E442","#359B73","#E69F00","#F748A5"))+
   scale_fill_manual(values=c("#0F6B99FF","#FFD8B2FF","#A3CC51FF","#8F7EE5FF","#990F0FFF"))+
   geom_bar(stat="identity",aes(x=UN_subregion, y=values, fill=Order), alpha = 0.7)+
   labs(x = "UN subregion", y = "Number of sites") +
-  scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) +
+  scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 10)) +
   scale_y_continuous(limits = c(0, 1600), breaks = c(0, 250, 500, 750, 1000, 1250, 1500))+
   theme_minimal()+
   theme(axis.text.x = element_text(angle = 50, size = 10, vjust = 0.5, hjust = 0.7),
         legend.position = "bottom",
-        #legend.key.size = unit(3,"mm"),
         legend.title = element_blank(),
-        #legend.text = element_text(size = 8)
         panel.grid = element_blank())
 
 
@@ -478,7 +396,7 @@ p_bar <- ggplot(data=mapdf2) +
 scatter_bar_mercator <- cowplot::plot_grid(p_map1.1,p_bar, ncol = 1, labels = c("(a)", "(b)"))
 
 # save plot (jpeg)
-ggsave("scatter_bar_mercator_UN.jpeg", device ="jpeg", path = plotDir, width=250, height=300, units="mm", dpi = 350)
+ggsave("FIGURE_1_scatter_bar_mercator_UN.jpeg", device ="jpeg", path = plotDir, width=250, height=300, units="mm", dpi = 350)
 
 
 
