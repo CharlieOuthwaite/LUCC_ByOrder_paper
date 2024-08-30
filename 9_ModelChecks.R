fda88e0da783280ec5f6fa49b10a76c0999f1073
Charlotte Outhwaite <Charlotte.Outhwaite@ioz.ac.uk>
2024-08-30 14:56:37 +0100

    reran and tidied

--- 9_ModelChecks.R
+++ 9_ModelChecks.R
@@ -50,7 +50,7 @@ model_data_ab <- readRDS(file = paste0(LUIDir,"model_data_ab.rds"))
 
 predictsSites <- readRDS(paste0(predictsDataDir,"PREDICTSSitesClimate_Data.rds"))
 
-#x <- mod_list1[2]
+#x <- mod_list1[1]
 
 # for the Order*LUI models
 for(x in mod_list1){
@@ -104,7 +104,7 @@ for(x in mod_list1){
     label1 <- paste0("P < 0.05 \nin ", round(perc_auto, 1), "% \nof studies")
     
     p4 <- ggplot(data = sa_test_vals ) +
-      geom_histogram(aes(x = sa_test_vals$`sa_test$P`)) +
+      geom_histogram(aes(x =`sa_test$P`)) +
       geom_vline(xintercept = 0.05, col = "red") +
       geom_text(aes(x = 0.9, y = 90, label = label1), size = 4, check_overlap = T) +
       theme_bw() +
@@ -231,7 +231,7 @@ for(x in mod_list2){
     label1 <- paste0("P < 0.05 \nin ", round(perc_auto, 1), "% \nof studies")
     
     p4 <- ggplot(data = sa_test_vals ) +
-      geom_histogram(aes(x = sa_test_vals$`sa_test$P`)) +
+      geom_histogram(aes(x = `sa_test$P`)) +
       geom_vline(xintercept = 0.05, col = "red") +
       geom_text(aes(x = 0.9, y = 90, label = label1), size = 4, check_overlap = T) +
       theme_bw() +
@@ -300,157 +300,6 @@ for(x in mod_list2){
   }
   
 }
-
-
-############################################################
-#                                                          #
-#            Model checks alternative packages             #
-#                                                          #
-############################################################
-
-## using the performance package
-# https://easystats.github.io/performance/articles/check_model.html
-library(performance)
-
-check_model(LUIRich$model)
-check_model(LUIAbund$model)
-
-check_model(sm3$model)
-check_model(am3$model)
-
-check_model(MeanAnomalyModelAbund$model)
-check_model(MeanAnomalyModelAbund2$model)
-
-check_model(MeanAnomalyModelRich$model)
-check_model(MeanAnomalyModelRich2$model)
-
-## using the DHARMa package
-# https://cran.r-project.org/web/packages/DHARMa/vignettes/DHARMa.html#interpreting-residuals-and-recognizing-misspecification-problems
-library(DHARMa)
-
-
-#### LUI only models ####
-
-# calculate residuals first to avoid rerunning within each function
-simulationOutput_richLUI <- simulateResiduals(fittedModel = LUIRich$model, plot = F)
-simulationOutput_abunLUI <- simulateResiduals(fittedModel = LUIAbund$model, plot = F)
-
-simulationOutput_richLUI_NO <- simulateResiduals(fittedModel = sm3$model, plot = F)
-simulationOutput_abunLUI_NO <- simulateResiduals(fittedModel = am3$model, plot = F)
-
-# Q-Q plot and residuals vs predicted
-plot(simulationOutput_richLUI)
-plot(simulationOutput_abunLUI)
-
-plot(simulationOutput_richLUI_NO)
-plot(simulationOutput_abunLUI_NO)
-
-# residuals vs predictor
-plotResiduals(simulationOutput_richLUI, form = model_data_sr$LUI)
-plotResiduals(simulationOutput_richLUI, form = model_data_sr$Order)
-plotResiduals(simulationOutput_abunLUI, form = model_data_ab$LUI)
-plotResiduals(simulationOutput_abunLUI, form = model_data_ab$Order)
-
-
-plotResiduals(simulationOutput_richLUI_NO, form = model_data_sr$LUI)
-plotResiduals(simulationOutput_abunLUI_NO, form = model_data_ab$LUI)
-
-# test uniformity
-testUniformity(simulationOutput_richLUI)
-testUniformity(simulationOutput_abunLUI)
-
-testUniformity(simulationOutput_richLUI_NO)
-testUniformity(simulationOutput_abunLUI_NO)
-
-# test outliers
-testOutliers(simulationOutput_richLUI)
-testOutliers(simulationOutput_abunLUI) # abundance has outliers but not clear where
-
-testOutliers(simulationOutput_richLUI_NO)
-testOutliers(simulationOutput_abunLUI_NO) # abundance has outliers but not clear where
-
-# test dispersion
-testDispersion(simulationOutput_richLUI) 
-testDispersion(simulationOutput_abunLUI) 
-testDispersion(simulationOutput_richLUI, type = "PearsonChisq", alternative = "greater") 
-testDispersion(simulationOutput_abunLUI, type = "PearsonChisq", alternative = "greater") 
-
-testDispersion(simulationOutput_richLUI_NO) 
-testDispersion(simulationOutput_abunLUI_NO) 
-testDispersion(simulationOutput_richLUI_NO, type = "PearsonChisq", alternative = "greater") 
-testDispersion(simulationOutput_abunLUI_NO, type = "PearsonChisq", alternative = "greater") 
-
-# test zero inflation
-testZeroInflation(simulationOutput_richLUI)
-testZeroInflation(simulationOutput_abunLUI)
-
-testZeroInflation(simulationOutput_richLUI_NO)
-testZeroInflation(simulationOutput_abunLUI_NO)
-
-#### LUI and STA models ####
-
-# calculate residuals first to avoid rerunning within each function
-simulationOutput_richLUISTA <- simulateResiduals(fittedModel = MeanAnomalyModelRich$model, plot = F)
-simulationOutput_abunLUISTA <- simulateResiduals(fittedModel = MeanAnomalyModelAbund$model, plot = F)
-
-
-simulationOutput_richLUISTA_NO <- simulateResiduals(fittedModel = MeanAnomalyModelRich2$model, plot = F)
-simulationOutput_abunLUISTA_NO <- simulateResiduals(fittedModel = MeanAnomalyModelAbund2$model, plot = F)
-
-# Q-Q plot and residuals vs predicted
-plot(simulationOutput_richLUISTA)
-plot(simulationOutput_abunLUISTA)
-
-plot(simulationOutput_richLUISTA_NO)
-plot(simulationOutput_abunLUISTA_NO)
-
-# residuals vs predictor
-plotResiduals(simulationOutput_richLUISTA, form = MeanAnomalyModelRich$data$LUI)
-plotResiduals(simulationOutput_richLUISTA, form = MeanAnomalyModelRich$data$Order)
-plotResiduals(simulationOutput_richLUISTA, form = MeanAnomalyModelRich$data$StdTmeanAnomalyRS)
-
-plotResiduals(simulationOutput_abunLUISTA, form = MeanAnomalyModelAbund$data$LUI)
-plotResiduals(simulationOutput_abunLUISTA, form = MeanAnomalyModelAbund$data$Order)
-plotResiduals(simulationOutput_abunLUISTA, form = MeanAnomalyModelAbund$data$StdTmeanAnomalyRS)
-
-plotResiduals(simulationOutput_richLUISTA_NO, form = MeanAnomalyModelRich2$data$LUI)
-plotResiduals(simulationOutput_richLUISTA_NO, form = MeanAnomalyModelRich2$data$StdTmeanAnomalyRS)
-
-plotResiduals(simulationOutput_abunLUISTA_NO, form = MeanAnomalyModelAbund2$data$LUI)
-plotResiduals(simulationOutput_abunLUISTA_NO, form = MeanAnomalyModelAbund2$data$StdTmeanAnomalyRS)
-
-
-# test uniformity
-testUniformity(simulationOutput_richLUISTA)
-testUniformity(simulationOutput_abunLUISTA)
-
-testUniformity(simulationOutput_richLUISTA_NO)
-testUniformity(simulationOutput_abunLUISTA_NO)
-
-# test outliers
-testOutliers(simulationOutput_richLUISTA)
-testOutliers(simulationOutput_abunLUISTA) 
-
-testOutliers(simulationOutput_richLUISTA_NO)
-testOutliers(simulationOutput_abunLUISTA_NO)
-
-# test dispersion
-testDispersion(simulationOutput_richLUISTA) 
-testDispersion(simulationOutput_abunLUISTA) 
-testDispersion(simulationOutput_richLUISTA, type = "PearsonChisq", alternative = "greater") 
-testDispersion(simulationOutput_abunLUISTA, type = "PearsonChisq", alternative = "greater") 
-
-testDispersion(simulationOutput_richLUISTA_NO) 
-testDispersion(simulationOutput_abunLUISTA_NO) 
-testDispersion(simulationOutput_richLUISTA_NO, type = "PearsonChisq", alternative = "greater") 
-testDispersion(simulationOutput_abunLUISTA_NO, type = "PearsonChisq", alternative = "greater") 
-
-# test zero inflation
-testZeroInflation(simulationOutput_richLUISTA)
-testZeroInflation(simulationOutput_abunLUISTA)
-
-testZeroInflation(simulationOutput_richLUISTA_NO)
-testZeroInflation(simulationOutput_abunLUISTA_NO)
 
 
 
